e465d88c9de19139e664291e1c78e6bf6008115c
ProstoARS <arsudakov@gmail.com>
2022-06-28 21:08:19 +0300

    1.1. Состояние нити. [#229175 #316792]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -15,7 +15,7 @@ public class ThreadState {
         first.start();
         second.start();
         while ((first.getState() != Thread.State.TERMINATED)
-                && (second.getState() != Thread.State.TERMINATED)) {
+                || (second.getState() != Thread.State.TERMINATED)) {
             System.out.println("First " + first.getState());
             System.out.println("Second " + second.getState());
         }
