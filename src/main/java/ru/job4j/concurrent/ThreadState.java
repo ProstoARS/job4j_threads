05c599e8ee68eb6afd2355a8e4b9a7e47ec51bff
ProstoARS <arsudakov@gmail.com>
2022-06-28 13:01:05 +0300

    1.1. Состояние нити. [#229175 #316792]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -1,7 +1,9 @@
 package ru.job4j.concurrent;
 
 public class ThreadState {
+
     public static void main(String[] args) {
+
         Thread first = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
