b4ccc961b06f2f745efcdfe5d4458bfd0e6bea31
ProstoARS <arsudakov@gmail.com>
2022-07-15 12:12:10 +0300

    3. Денежные переводы AccountStorage [#1104 #322246]

--- src/main/java/ru/job4j/cash/AccountStorage.java
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -14,58 +14,42 @@ public class AccountStorage {
 
     public boolean add(Account account) {
         synchronized (accounts) {
-            if (!accounts.containsKey(account.getId())) {
-                accounts.put(account.getId(), account);
-                return true;
-            }
+            return accounts.putIfAbsent(account.getId(), account) == null;
         }
-        System.out.println("account with current Id already exist!");
-        return false;
     }
 
     public boolean update(Account account) {
         synchronized (accounts) {
-            if (accounts.containsKey(account.getId())) {
-                accounts.replace(account.getId(), account);
-                return true;
-            }
+            return accounts.replace(account.getId(), account) == null;
         }
-        System.out.println("this account does not exist!");
-        return false;
     }
 
     public boolean delete(int id) {
         synchronized (accounts) {
-            accounts.remove(id);
-            return true;
+            return accounts.remove(id) == null;
         }
     }
 
     public Optional<Account> getById(int id) {
         synchronized (accounts) {
-            if (accounts.containsKey(id)) {
-                return Optional.of(accounts.get(id));
-            }
+                return Optional.ofNullable(accounts.get(id));
         }
-        return Optional.empty();
     }
 
     public boolean transfer(int fromId, int toId, int amount) {
-
-        if (getById(fromId).isPresent() && getById(toId).isPresent()) {
-            if (getById(fromId).get().getAmount() >= amount) {
-                synchronized (accounts) {
+        synchronized (accounts) {
+            Optional<Account> from = getById(fromId);
+            Optional<Account> to = getById(toId);
+        if (from.isPresent() && to.isPresent()
+            && from.get().getAmount() >= amount) {
                     update(new Account(getById(fromId).get().getId(),
                             getById(fromId).get().getAmount() - amount));
                     update(new Account(getById(toId).get().getId(),
                             getById(toId).get().getAmount() + amount));
                     return true;
-                }
             } else {
                 System.out.println("there are not enough funds on the account!");
             }
-        } else {
-            System.out.println("accounts not found!");
         }
         return false;
     }
